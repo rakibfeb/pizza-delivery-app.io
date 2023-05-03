4512565b049ee52197b6360b1444fffe7d2af1b9
rakibfeb <rakib8feb@gmail.com>
2023-05-03 17:17:53 +0530

    created server

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const ejs = require('ejs')
+const expressLayout = require('express-ejs-layouts')
+const path = require('path')
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.get('/',(req,res) => {
+    res.render('home')
+})
+
+// set template engine
+app.use(expressLayout)
+app.set('views', path.join(__dirname,'/resources/views'))
+app.set('view engine', 'ejs')
+
+// server
+app.listen(PORT,()=>{
+    console.log(`server listening on port ${PORT}`)
+})
