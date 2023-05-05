4bfa5633f2e28b4af77f065dcae45f360c2225c6
rakibfeb <rakib8feb@gmail.com>
2023-05-05 11:05:29 +0530

    setup laravel mix and tailwind css

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 3000
 
+//Assets
+app.use(express.static('public'))
+
 app.get('/',(req,res) => {
     res.render('home')
 })
-
 // set template engine
 app.use(expressLayout)
 app.set('views', path.join(__dirname,'/resources/views'))
