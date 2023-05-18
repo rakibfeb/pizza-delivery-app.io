fa6ff7b8e165d54ebde1619803772d642e112dc5
rakibfeb <rakib8feb@gmail.com>
2023-05-18 13:29:01 +0530

    add to cart functionlity

--- routes/web.js
+++ routes/web.js
@@ -3,14 +3,15 @@ const authController = require('../app/http/controllers/authController')
 const cartController = require('../app/http/controllers/customers/cartController')
 
 function initRoutes(app){
-// home page server setup
-app.get('/',homeController().index)
-// cart page server setup
-app.get('/cart',cartController().index)
-// login page server setup
-app.get('/login',authController().login)
-// register page server setup
-app.get('/register',authController().register)
+        // home page server setup
+        app.get('/',homeController().index)
+        // login page server setup
+        app.get('/login',authController().login)
+        // register page server setup
+        app.get('/register',authController().register)
+        // cart page server setup
+        app.get('/cart',cartController().index)
+        app.post('/update-cart', cartController().update)
 }
 
 module.exports = initRoutes
--- server.js
+++ server.js
@@ -1,13 +1,47 @@
+require('dotenv').config()
 const express = require('express')
 const ejs = require('ejs')
 const expressLayout = require('express-ejs-layouts')
 const path = require('path')
-
 const app = express()
 const PORT = process.env.PORT || 3000
+const mongoose = require('mongoose')
+const session = require('express-session')
+const flash = require('express-flash')
+const MongoDbStore = require('connect-mongo')
 
+// Database connection
+const url = 'mongodb://localhost/pizza';
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+connection.on('error', (error) => {
+  console.log('Database connection error:', error);
+});
+connection.once('open', () => {
+  console.log('Database connection successful');
+});
+
+// session config
+app.use(session({
+  secret: process.env.COOKIE_SECRET,
+  resave: false,
+  store: MongoDbStore.create({ 
+    client: connection.getClient()
+  }),
+  saveUninitialized: false,
+  cookie:{maxAge: 1000*60*60*24} // 24 hours
+}))
+
+app.use(flash())
 //Assets
 app.use(express.static('public'))
+app.use(express.json())
+
+// global middleware 
+app.use ((req,res,next) => {
+  res.locals.session = req.session
+  next()
+})
 
 // set template engine
 app.use(expressLayout)
