4c1ac0cadda0d94fbc1ef51182a842f3cd81d3da
rakibfeb <rakib8feb@gmail.com>
2023-05-21 17:36:34 +0530

    passport & order tracker

--- server.js
+++ server.js
@@ -1,57 +1,93 @@
 require('dotenv').config()
 const express = require('express')
-const ejs = require('ejs')
-const expressLayout = require('express-ejs-layouts')
-const path = require('path')
 const app = express()
-const PORT = process.env.PORT || 3000
+const ejs = require('ejs')
+const path = require('path')
+const expressLayout = require('express-ejs-layouts')
+const PORT = process.env.PORT || 3300
 const mongoose = require('mongoose')
 const session = require('express-session')
 const flash = require('express-flash')
-const MongoDbStore = require('connect-mongo')
+const MongoDbStore = require('connect-mongo')(session)
+const passport = require('passport')
+const Emitter = require('events')
 
 // Database connection
-const url = 'mongodb://localhost/pizza';
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, useFindAndModify : true });
 const connection = mongoose.connection;
-connection.on('error', (error) => {
-  console.log('Database connection error:', error);
-});
 connection.once('open', () => {
-  console.log('Database connection successful');
+    console.log('Database connected...');
+}).catch(err => {
+    console.log('Connection failed...')
 });
 
-// session config
+
+// Session store
+let mongoStore = new MongoDbStore({
+                mongooseConnection: connection,
+                collection: 'sessions'
+            })
+
+// Event emitter
+const eventEmitter = new Emitter()
+app.set('eventEmitter', eventEmitter)
+
+// Session config
 app.use(session({
-  secret: process.env.COOKIE_SECRET,
-  resave: false,
-  store: MongoDbStore.create({ 
-    client: connection.getClient()
-  }),
-  saveUninitialized: false,
-  cookie:{maxAge: 1000*60*60*24} // 24 hours
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    store: mongoStore,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 } // 24 hour
 }))
 
+// Passport config
+const passportInit = require('./app/config/passport')
+passportInit(passport)
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.use(flash())
-//Assets
+// Assets
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
-// global middleware 
-app.use ((req,res,next) => {
-  res.locals.session = req.session
-  next()
+// Global middleware
+app.use((req, res, next) => {
+    res.locals.session = req.session
+    res.locals.user = req.user
+    next()
 })
-
-// set template engine
+// set Template engine
 app.use(expressLayout)
-app.set('views', path.join(__dirname,'/resources/views'))
+app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
 
-// requiring from web.js as a function and calling function
 require('./routes/web')(app)
-
-// server
-app.listen(PORT,()=>{
-    console.log(`server listening on port ${PORT}`)
+app.use((req, res) => {
+    res.status(404).render('errors/404')
 })
+
+const server = app.listen(PORT , () => {
+            console.log(`Listening on port ${PORT}`)
+        })
+
+// Socket
+
+const io = require('socket.io')(server)
+io.on('connection', (socket) => {
+      // Join
+      socket.on('join', (orderId) => {
+        socket.join(orderId)
+      })
+})
+
+eventEmitter.on('orderUpdated', (data) => {
+    io.to(`order_${data.id}`).emit('orderUpdated', data)
+})
+
+eventEmitter.on('orderPlaced', (data) => {
+    io.to('adminRoom').emit('orderPlaced', data)
+})
+
